d0ce6268e92c0bb2dd355ecf794aff798d75f191
Chris Kay <chriskay25@gmail.com>
2019-04-14 22:32:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
     elsif array.length == 2 
       array.join(" and ")
     elsif array.length > 2
-      array << "and"
-      array.join(", ")
+	  array.insert(-2,"and ")
+	  last = array.pop
+	  array.join(", ") << last
   end
 end
